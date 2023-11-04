82934af303b68cdeee06c0dad6925a6566d03c20
Aleksey Poznyak <ddoslang@gmail.com>
2023-11-05 00:09:44 +0200

    almost done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1 class="container__title">Прочешем места? &#127866;</h1>
+    <Search />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+import Search from "./components/Search.vue";
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 700px;
+  margin: 80px auto;
+  padding: 40px 40px;
+  box-shadow: 0 3px 10px 2px #0000001a;
+  border-radius: 16px;
 }
 </style>
